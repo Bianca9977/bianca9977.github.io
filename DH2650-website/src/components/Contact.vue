774185eb60ba14530ff3662ff2e353789609fd46
yangl56 <yangl56@mcmaster.ca>
2022-05-17 18:47:48 +0200

    team/contact grid created, need to add images and reformat text

--- DH2650-website/src/components/Contact.vue
+++ DH2650-website/src/components/Contact.vue
@@ -1,35 +1,67 @@
 <template>
     <section id="contact" class = "contact">
-        <div class="container mx-auto p-5">
-            <h3>
+        <div class="container mx-auto p-5 h-full flex flex-col justify-center">
+            <h3 class="xl:text-6xl text-center">
                 The Team
             </h3>
-            <p>
-                Bianca Budulacu
-                budulacu@kth.se
-            </p>
-            <p>
-                August Danell
-                aughak@kth.se
-            </p>
-            <p>
-                Andreas Wrife
-                awrife@kth.se
-            </p>
-            <p>
-                Max Wippich
-                wippich@kth.se
-            </p>
-            <p>
-                Laura Yang
-                laura@yang.ca
-            </p>
+            <div class="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-5 mt-16">
+                <div v-for="(feature, index) in features" :key="index">
+                    {{ feature.photo }} {{ feature.name }} - {{ feature.role }}
+                </div>
+            </div>
         </div>
     </section>
 </template>
 
 <script setup>
+var bianca = require('../assets/bianca.jpg');
+var august = require('../assets/bianca.jpg');
+var max = require('../assets/bianca.jpg');
+var andreas = require('../assets/bianca.jpg');
+var laura = require('../assets/bianca.jpg');
 
+export default {
+    data() {
+        return {
+        currentElement: null,
+        features: [
+            {
+                name: "Bianca Budulacu",
+                role: "role",
+                photo: bianca
+            },
+            {
+                name: "August Danell",
+                role: "role",
+                photo: august
+            },
+            {
+                name: "Max Wippich",
+                role: "role",
+                photo: max
+            },
+            {
+                name: "Andreas Wrife",
+                role: "role",
+                photo: andreas
+            },
+            {
+                name: "Laura Yang",
+                role: "role",
+                photo: laura
+            }
+        ]
+        };
+    },
+
+    created: function() {
+        var self = this;
+
+    },
+
+    methods: {
+    }
+}
 </script>
 
 
