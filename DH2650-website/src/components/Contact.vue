e47a631e3b2591dace22a8a3157320dc5b2d0502
yangl56 <yangl56@mcmaster.ca>
2022-05-20 14:41:41 +0200

    got respective team member photos up, need to resize

--- DH2650-website/src/components/Contact.vue
+++ DH2650-website/src/components/Contact.vue
@@ -6,8 +6,7 @@
             </h3>
             <div class="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-5 mt-16">
                 <div v-for="(member, index) in team" :key="index">
-                    <img src="../assets/bianca.jpg">
-                    <img :src="member.photopath" :alt="member.name"> <br> {{ member.name }} <br> {{ member.role }}
+                    <img :src="member.photo" :alt="member.name"> <br> {{ member.name }} <br> {{ member.role }}
                 </div>
             </div>
         </div>
@@ -30,36 +29,36 @@ export default {
                 name: "Bianca Budulacu",
                 role: "Graphics Lead",
                 photo: require('../assets/bianca.jpg'),
-                photoname: "bianca",
-                photopath: "/assets/bianca.jpg"
+                photoname: "bianca.jpg",
+                photopath: "../assets/bianca.jpg"
             },
             {
                 name: "August Danell",
                 role: "Business Reporter Lead",
                 photo: require('../assets/august2.png'),
-                photoname: "august2",
-                photopath: "../assets/august2.jpg"
+                photoname: "august2.png",
+                photopath: "../assets/august2.png"
                 
             },
             {
                 name: "Max Wippich",
                 role: "Game Development Lead",
                 photo: require('../assets/max.jpg'),
-                photoname: "max",
+                photoname: "max.jpg",
                 photopath: "../assets/max.jpg"
             },
             {
                 name: "Andreas Wrife",
                 role: "Project Management Lead",
                 photo: require('../assets/andreas.jpg'),
-                photoname: "andreas",
+                photoname: "andreas.jpg",
                 photopath: "../assets/andreas.jpg"
             },
             {
                 name: "Laura Yang",
                 role: "Jack of All Trades",
                 photo: require('../assets/laura.jpg'),
-                photoname: "laura",
+                photoname: "laura.jpg",
                 photopath: "../assets/laura.jpg"
             }
         ]
