fa1bbf664f74a2aace0435a89f14656882af8fa1
Bianca9977 <bianca99776@gmail.com>
2022-05-21 10:18:29 +0200

    Dist test

--- DH2650-website/src/components/Contact.vue
+++ DH2650-website/src/components/Contact.vue
@@ -2,7 +2,7 @@
     <section id="contact" class = "contact">
         <div class="container mx-auto p-5 h-full flex flex-col justify-center">
             <h3 class="xl:text-6xl text-center">
-                The Team
+                The Team Test
             </h3>
             <div class="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-5 mt-16">
                 <div v-for="(member, index) in team" :key="index">
@@ -28,7 +28,7 @@ export default {
             {
                 name: "Bianca Budulacu",
                 role: "Graphics Lead",
-                photo: require('../assets/bianca.jpg'),
+                photo: "img/bianca.jpg",
                 photoname: "bianca.jpg",
                 photopath: "../assets/bianca.jpg"
             },
